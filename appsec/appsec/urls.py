bbf285bd3bb6e575995c9dcb2ba73a47314ce569
Parsons, Adam <Adam.Parsons@Pearson.com>
2015-03-11 10:51:02 -0500

    Added root dir redirect

--- appsec/appsec/urls.py
+++ appsec/appsec/urls.py
@@ -3,17 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import RedirectView
 
 from tracker.views import dashboard_personal, management_services, management_users
 
 base_urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(pattern_name='tracker:dashboard.personal')),
+
     url(r'^tracker/', include('tracker.urls', namespace="tracker")),
-
-    #url(r'^manage/services/$', management_services, name='management.services'),
-    #url(r'^manage/users/$', management_users, name='management.users'),
-
     url(r'^accounts/', include('accounts.urls', namespace="accounts")),
-
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
