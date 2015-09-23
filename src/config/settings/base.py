1c2c9058f5b908a323e309788dd18bf3ddbaaf6b
aaron <aaron@mac-groveapple.local>
2015-09-23 10:44:29 -0700

    Added django_admin_bootstrapped module to style the admin interface

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -27,6 +28,7 @@ INSTALLED_APPS = (
 
     'rest_framework',
     'rest_framework.authtoken',
+
     'django_filters',
     'widget_tweaks',
 
