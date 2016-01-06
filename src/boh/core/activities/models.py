8518dc6c26cf5c314dd27102d0e4fa4cfe916b36
Adam Parsons <adam@aparsons.net>
2016-01-06 17:07:50 -0600

    Scaffolding activities

--- src/boh/core/activities/models.py
+++ src/boh/core/activities/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from boh.core import behaviors
+
+from . import managers, querysets
+
+
+class Action(behaviors.Timestampable, models.Model):
+    """Describes an actor performing a verb on an optional target."""
+
+    objects = managers.ActionManager.from_queryset(querysets.ActionQuerySet)()
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = _('Action')
+        verbose_name_plural = _('Actions')
+
+
+class Follow(behaviors.Timestampable, models.Model):
+    """Allows users to follow the actions of actors."""
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = _('Follow')
