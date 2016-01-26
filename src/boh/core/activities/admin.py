b9edac1e61f3f34ee576c40f437d2ab3456afab0
Adam Parsons <adam@aparsons.net>
2016-01-26 15:57:11 -0600

    Added fieldsets

--- src/boh/core/activities/admin.py
+++ src/boh/core/activities/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from . import models
 
 
 class EventAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
+    fieldsets = (
+        (_('Actor'), {
+            'fields': (('actor_type', 'actor_id'),)
+        }),
+        (_('Verb'), {
+            'fields': ('verb',)
+        }),
+        (_('Action'), {
+            'fields': (('action_type', 'action_id'),)
+        }),
+        (_('Preposition'), {
+            'fields': ('preposition',)
+        }),
+        (_('Target'), {
+            'fields': (('target_type', 'target_id'),)
+        }),
+        (_('Metadata'), {
+            'fields': ('public', 'created', 'modified')
+        }),
+    )
     list_display = ['__str__', 'actor', 'verb', 'action', 'preposition', 'target', 'public', 'created']
     list_filter = ['public', 'created']
     readonly_fields = ['created', 'modified']
@@ -14,6 +35,17 @@ admin.site.register(models.Event, EventAdmin)
 
 class FollowAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
+    fieldsets = (
+        (_('User'), {
+            'fields': ('user',)
+        }),
+        (_('Actor'), {
+            'fields': (('actor_type', 'actor_id'),)
+        }),
+        (_('Metadata'), {
+            'fields': ('created', 'modified')
+        }),
+    )
     list_display = ['__str__', 'user', 'actor', 'created']
     list_filter = ['created']
     readonly_fields = ['created', 'modified']
