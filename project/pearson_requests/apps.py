66b758648ad6a83e1c74a8f034e9c19faebd6896
Adam Parsons <adam.parsons@pearson.com>
2016-04-20 13:52:47 -0500

    removed pearson_requests

--- project/pearson_requests/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PearsonRequestsConfig(AppConfig):
-    name = 'pearson_requests'
-    verbose_name = "Service Requests (Pearson)"
--- project/pearson_requests/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def external_request_success(request):
-    return render(request, 'pearson_requests/wizards/external_request/success.html', {})
