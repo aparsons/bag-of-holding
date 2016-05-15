4bf3b0f7df595f3ec3401c674b9c73e638e890bb
Adam Parsons <adam@parsons.cloud>
2016-05-15 14:28:09 -0500

    Added custom fields to applications api response

--- project/boh_api/serializers.py
+++ project/boh_api/serializers.py
@@ -15,14 +15,26 @@ class OrganizationSerializer(serializers.ModelSerializer):
         model = models.Organization
 
 
+class ApplicationCustomFieldValueSerializer(serializers.ModelSerializer):
+    key = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ['id', 'key', 'value']
+        model = models.ApplicationCustomFieldValue
+
+    def get_key(self, custom_field_value):
+        return custom_field_value.custom_field.key
+
+
 class ApplicationSerializer(serializers.ModelSerializer):
     threadfix_metrics = serializers.SerializerMethodField()
     organization = OrganizationSerializer(read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+    custom_fields = ApplicationCustomFieldValueSerializer(source='applicationcustomfieldvalue_set', many=True, read_only=True)
 
     class Meta:
         model = models.Application
-        fields = ['id', 'organization', 'name', 'description', 'business_criticality', 'platform', 'lifecycle', 'origin', 'user_records', 'revenue', 'external_audience', 'internet_accessible', 'threadfix_metrics', 'tags', 'people']
+        fields = ['id', 'organization', 'name', 'description', 'business_criticality', 'platform', 'lifecycle', 'origin', 'user_records', 'revenue', 'external_audience', 'internet_accessible', 'threadfix_metrics', 'tags', 'custom_fields', 'people']
 
     def get_threadfix_metrics(self, application):
         try:
