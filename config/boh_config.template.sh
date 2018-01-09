8b9e8071dc21baea03664cf288d08585a160735e
Yannarak Wannasai <yannarak@mercari.com>
2018-01-09 13:17:01 +0900

    - Added configuration file template

---
+++ config/boh_config.template.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+export BOH_SECRET_KEY='5g3bqc$yp(+$obzr^z2=49grt%_ke5xp6i#5f$v17v7aldr!nr'
+export BOH_DB_NAME='boh'
+export BOH_DB_USER='boh_user'
+export BOH_DB_PASSWORD='M59uw1kXNcudnlgn'
+export BOH_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY='73666118989-5nnad8f14cp9t07npinuia40rm5mn0o1.apps.googleusercontent.com'
+export BOH_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET='Azg5qBBSUuH9NbgULNW7tef2'
